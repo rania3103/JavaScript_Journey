b1f4f3edfe40d86af4145397569adef9f98646eb
rania3103 <raniaabassi33@gmail.com>
2024-01-02 16:19:22 +0100

    dealing with variables

---
+++ variables/exercice.js
@@ -0,0 +1,8 @@
+let full_name = "rania abassi";
+let age = 20;
+let is_student = true;
+
+document.getElementById("p1").textContent = `Name: ${full_name}`;
+document.getElementById("p2").textContent = `Age: ${age}`;
+document.getElementById("p3").textContent = `Are you student?: ${is_student}`;
+document.getElementById("h1").style.color="red";
---
+++ variables/index.js
@@ -0,0 +1,19 @@
+// declare variables and assign values
+//number
+let age = 20;
+let price = 10.99;
+// print output the console
+// you have to use backtick ``
+console.log(`you are ${age} years old`);
+console.log(`the price is  ${price}`);
+//print type of variables
+console.log(typeof age);
+console.log(typeof price);
+//string
+let firstname = "Rania";
+console.log(typeof firstname);
+console.log(`my name is ${firstname}`);
+//boolean
+let available = true;
+console.log(typeof available);
+console.log(`available?:  ${available}`);
