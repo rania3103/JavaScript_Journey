6fb9f878c1023ade56ddb2d30c33e07a6bf42539
rania3103 <raniaabassi33@gmail.com>
2024-01-02 16:48:07 +0100

    arithmetic operators

---
+++ arithmetic_operator/index.js
@@ -0,0 +1,26 @@
+let students = 40;
+students += 1; // same as students = students + 1;
+console.log(students);
+
+students -= 1; // same as students = students - 1;
+console.log(students);
+
+students *= 2; // same as students = students * 2;
+console.log(students);
+
+students /= 2; // same as students = students / 2;
+console.log(students);
+
+students **= 2; // same as students = students ** 2;
+console.log(students);
+
+students %= 2; // same as students = students % 2;
+console.log(students);
+
+//increment by one
+let number = 4;
+number++;
+console.log(number);
+//deincrement by one
+number--;
+console.log(number);
