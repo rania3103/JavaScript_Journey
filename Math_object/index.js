ef475f3c9a076b03e322fb6a21ab92218689c031
rania3103 <raniaabassi33@gmail.com>
2024-01-03 10:15:51 +0100

    math

---
+++ Math_object/index.js
@@ -0,0 +1,53 @@
+let x = 3.21;
+let y = 2;
+let z;
+//round
+z = Math.round(x);
+console.log(z);
+// floor
+x = 3.99;
+z = Math.floor(x);
+console.log(z);
+// ceil
+x = 3.99;
+z = Math.ceil(x);
+console.log(z);
+// trunc
+x = 3.21;
+z = Math.trunc(x);
+console.log(z);
+// pow
+x = 3;
+z = Math.pow(x, y);
+console.log(z);
+// sqrt
+x = 9;
+z = Math.sqrt(x);
+console.log(z);
+// sqrt
+x = 45;
+z = Math.sin(x);
+console.log(z);
+// tan
+z = Math.tan(x);
+console.log(z);
+// abs
+x = -5;
+z = Math.abs(x);
+console.log(z);
+// sign
+x = -5;
+z = Math.sign(x);
+console.log(z);
+// sign
+x = 8;
+z = Math.sign(x);
+console.log(z);
+// max - min
+x = 8;
+z= -1;
+y = 3;
+let max = Math.max(x, y, z);
+console.log(max);
+let min = Math.min(x, y, z);
+console.log(min);
