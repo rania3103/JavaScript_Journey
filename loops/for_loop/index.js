14cd70afb8f1a4058645855170dec45f9ad14710
rania3103 <raniaabassi33@gmail.com>
2024-01-03 14:25:07 +0100

    for loop

---
+++ loops/for_loop/index.js
@@ -0,0 +1,23 @@
+for (let index = 0; index < 2; index++) {
+    console.log("hello rania");
+    console.log(index);
+}
+////////
+for (let i = 10; i >= 0; i--) {
+    console.log("happy new year rania");
+    console.log(i);
+}
+//////
+for (let index = 1; index <= 20; index++) {
+    if (index == 11)
+    {
+        continue;
+    }else if (index == 15)
+    {
+        break;
+    }
+    else{
+        console.log(index);
+    }
+    
+}
