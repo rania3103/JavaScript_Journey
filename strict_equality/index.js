1af64c4b7b5c0bab9edabea5aae39b23ac7159ec
rania3103 <raniaabassi33@gmail.com>
2024-01-03 13:52:49 +0100

    compare

---
+++ strict_equality/index.js
@@ -0,0 +1,22 @@
+// = assignment operator
+// == compare if values are equal
+// === compare if values and types are eqaul
+// != inequality
+// !== strcit inequality operator
+const pi = 3.14;
+if (pi == "3.14")
+{
+    console.log("that is pi");
+}
+else
+{
+    console.log("that is not pi");
+}
+if (pi === "3.14")
+{
+    console.log("that is pi");
+}
+else
+{
+    console.log("that is not pi");
+}
