fa97d4407c2b87dcb1ca6ce7e2c955367b3b4721
rania3103 <raniaabassi33@gmail.com>
2024-01-03 12:44:12 +0100

    ternary operation

---
+++ ternary_operator/index.js
@@ -0,0 +1,11 @@
+let age = 20;
+let message = age >= 18 ? "you are an adult": "you are minor";
+console.log(message);
+///
+let time = 12;
+let greeting = time < 12 ? "good morning" :"good afternoon";
+console.log(greeting);
+///
+let is_student= true;
+let check = is_student? "you are a student" :"you are not a student";
+console.log(check);
