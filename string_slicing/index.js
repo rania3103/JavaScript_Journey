130d6314d1c53096528d73757c2b7651c4ce1c76
rania3103 <raniaabassi33@gmail.com>
2024-01-03 13:20:40 +0100

    string slicing

---
+++ string_slicing/index.js
@@ -0,0 +1,7 @@
+//////slice
+const fullname ="rania abassi";
+let firstname= fullname.slice(0, fullname.indexOf(" "));
+console.log(firstname);
+//////substring
+let lastname = fullname.substring(fullname.indexOf(" ") + 1,);
+console.log(lastname);
